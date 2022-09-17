0fbed13c640cdb4835ba896fd564ffcabcac8dbb
ttoomas <folprecht.to@gmail.com>
2022-09-17 21:26:51 +0200

    two more candies added

--- res/js/main.js
+++ res/js/main.js
@@ -11,12 +11,12 @@ let i = 0;
 
 // Video Box
 box.addEventListener('click', () => {
-    if(!mainVideoTop.hasAttribute('disableVideo')){
+    if(!mainVideoTop.hasAttribute('disable-video')){
         mainVideoTop.play();
         mainVideoBottom.play();
 
-        mainVideoTop.setAttribute('disableVideo', '');
-        mainClick.setAttribute('disable', '');
+        mainVideoTop.setAttribute('disable-video', '');
+        mainClick.setAttribute('disable-count', '');
 
         plusOne();
 
@@ -24,7 +24,7 @@ box.addEventListener('click', () => {
             mainCandy.style.top = "-85px";
 
             setTimeout(() => {
-                mainClick.removeAttribute('disable');
+                mainClick.removeAttribute('disable-count');
             }, 350);
 
         }, 1600);
@@ -33,7 +33,57 @@ box.addEventListener('click', () => {
 
 // 1st Candy
 mainCandy.addEventListener('click', () => {
-    if(!mainClick.hasAttribute('disable')){
+    if(!mainClick.hasAttribute('disable-count') && i < 15){
+        plusOne();
+    }
+})
+
+// 2nd Candy - Fidorka
+const mainFidorka = document.querySelector('.main__fidorka');
+
+function fidorkaSwap(){
+    mainClick.setAttribute('disable-count', '');
+
+    mainCandy.style.left = "120%";
+    mainCandy.style.opacity = "0";
+
+    setTimeout(() => {
+        mainFidorka.style.top = "-88px";
+        
+        setTimeout(() => {
+            mainClick.removeAttribute('disable-count');
+        }, 350);
+
+    }, 400);
+}
+
+mainFidorka.addEventListener('click', () => {
+    if(!mainClick.hasAttribute('disable-count')){
+        plusOne();
+    }
+})
+
+// 3rd candy - Tatranka
+const mainTatranka = document.querySelector('.main__tatranka');
+
+function tatrankaSwap(){
+    mainClick.setAttribute('disable-count', '');
+
+    mainFidorka.style.left = "120%";
+    mainFidorka.style.opacity = "0";
+
+    setTimeout(() => {
+        mainTatranka.style.top = "-130px";
+
+        setTimeout(() => {
+            mainClick.removeAttribute('disable-count');    
+        }, 350);
+
+    }, 400);
+}
+
+mainTatranka.addEventListener('click', () => {
+    if(!mainClick.hasAttribute('disable-count')){
         plusOne();
     }
 })
@@ -41,13 +91,16 @@ mainCandy.addEventListener('click', () => {
 
 // Function plusOne to add one number to var. i and rewrite html
 function plusOne(){
-    i++
+    // i++;
+    // Only for development !!!
+    i += 5;
+
     countText.innerText = i;
 
     if(i == 15){
-        console.log('change');
+        fidorkaSwap();
     }
     else if(i == 25){
-        console.log('another change');
+        tatrankaSwap();
     }
 }
