e3f72a590ee6b42009e501cecd092fff15d7351e
ttoomas <folprecht.to@gmail.com>
2022-09-17 11:58:17 +0200

    box open + counting clicks

---
+++ res/js/main.js
@@ -0,0 +1,37 @@
+const box = document.querySelector('.main__videoBox');
+const clickCountText = document.querySelector('.main__times');
+
+let i = 0;
+
+
+// Box Video
+box.addEventListener('click', () => {
+    if(!box.hasAttribute('disable')){
+        box.play();
+        box.setAttribute('disable', '');
+        box.setAttribute('playing', '');
+
+        plusOne();
+
+        setTimeout(() => {
+            box.removeAttribute('playing')
+        }, 1900);
+    }
+    else if(!box.hasAttribute('playing')){
+        plusOne();
+    }
+})
+
+
+// Function plusOne to add one number to var. i and rewrite html
+function plusOne(){
+    if(i == 19){
+        console.log('change');
+    }
+    else if(i == 29){
+        console.log('another change');
+    }
+
+    i++
+    clickCountText.innerText = i;
+}
